5fcdb27166d2041df983e6b4561cd71a4b680e06
CHATHURANGA K.M.P <fas222282@kul.wyb.ac.lk>
2026-04-26 12:48:03 +0530

    Add authentication and authorization middleware

---
+++ backend/middleware/auth.js
@@ -0,0 +1,46 @@
+const { verifyToken } = require('../config/jwt');
+
+const auth = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization?.split(' ')[1];
+
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: 'No token provided'
+      });
+    }
+
+    const decoded = verifyToken(token);
+
+    if (!decoded) {
+      return res.status(401).json({
+        success: false,
+        message: 'Invalid or expired token'
+      });
+    }
+
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: 'Authentication error',
+      error: error.message
+    });
+  }
+};
+
+const authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({
+        success: false,
+        message: 'You are not authorized to access this resource'
+      });
+    }
+    next();
+  };
+};
+
+module.exports = { auth, authorize };
